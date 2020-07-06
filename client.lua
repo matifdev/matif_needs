d61ecd2ef6ac6a426241e7451eb2d4429d94b7a5
matifdev <Martimfreitas2012@gmail.com>
2020-07-06 04:52:53 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -20,8 +20,6 @@ local Keys = {
 	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
 }
 
-RegisterNetEvent('matif_needs:keyPress')
-
 lastPresses = {}
 
 Citizen.CreateThread(function() 
